9c43f6a30cb6dd762500fadc55bef02c41f1695d
y-matsuki <yuto.matsuki@recochoku.co.jp>
2015-09-14 10:24:10 +0900

    bug fix

--- extract_keywords.py
+++ extract_keywords.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from multiprocessing import Process
+import time, math
 
 import rethinkdb as r
 from  rethinkdb import ReqlRuntimeError
@@ -13,33 +13,32 @@ try:
 except ReqlRuntimeError:
     pass
 
-def remove_blank(keywords):
-    while True:
-        try:
-            keywords.remove("")
-        except ValueError:
-            break
 
-def extract_keyword(raw_text):
+def extract_keyword(text):
     tagger = MeCab.Tagger()
     keywords = []
-    for text in raw_text.split():
-        if text.startswith('@') or text.startswith('#') or text.startswith('http'):
-            continue;
-        encoded_text = text.encode('utf-8')
-        node = tagger.parseToNode(encoded_text).next
-        tmp = []
-        while node:
-            if node.feature.split(',')[0] == "名詞":
-                tmp.append(node.surface)
-            if node.feature.split(',')[0] != "名詞":
-                if len(tmp) > 1:
-                    keywords.append("".join(tmp))
-                del tmp[:]
-            node = node.next
-        if len(tmp) > 1:
-            keywords.append("".join(tmp))
-        remove_blank(keywords)
+    if text.startswith("I'm at "):
+        return []
+    splited_text = text.split():
+    splited_text = [ v for v in splited_text if not v.startswith('@') ]
+    splited_text = [ v for v in splited_text if not v.startswith('#') ]
+    splited_text = [ v for v in splited_text if not v.startswith('http') ]
+    text = ' '.join(splited_text)
+    encoded_text = text.encode('utf-8')
+    node = tagger.parseToNode(encoded_text).next
+    tmp = []
+    while node:
+        if node.feature.split(',')[0] == "名詞":
+            tmp.append(node.surface)
+        if node.feature.split(',')[0] != "名詞":
+            if len(tmp) > 1:
+                keywords.append("".join(tmp))
+            del tmp[:]
+        node = node.next
+    if len(tmp) > 1:
+        keywords.append("".join(tmp))
+    keywords = [ v for v in keywords if not v == '' ]
+    keywords = [ v for v in keywords if not v.startswith('http') ]
     return keywords
 
 def handle_stream():
@@ -53,7 +52,8 @@ def handle_stream():
         for keyword in keywords:
             json_data = {
                 'word': unicode(keyword, ('utf-8')),
-                'text': text
+                'text': text,
+                'time': math.floor(time.time() * 1000)
             }
             r.db('mecab').table('dict').insert(json_data).run()
         print('----')
